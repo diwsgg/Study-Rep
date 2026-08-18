c964841fd04bb1171e3c6a68c41d525e45871185
DWgg <308503815+diwsgg@users.noreply.github.com>
2026-08-18 12:28:48 -0600

    Adding changes to the array options

--- Data Structures/Codes/StaticArray.java
+++ Data Structures/Codes/StaticArray.java
@@ -1,56 +1,74 @@
+import java.util.Scanner;
 
-public class Main {
+public class StaticArray {
+
+    static int count = 0; //Track how many slots are filled 
     public static void main(String[] args) {
-        int  index = -1;
-        //WE will create an array of integers and print its size
+        //int  index = -1;
 
+        Scanner scanner = new Scanner(System.in);
+
+        //WE will create an array of integers and print its size
         System.out.println("Enter the size of the array: ");
         //Ask to the user to enter the size of the array
-        int size = Integer.parseInt(System.console().readLine());
+        int size = Integer.parseInt(scanner.nextLine());
 
         //Now we create the array with the size entered by the user
         int[] array = new int[size];
 
         //Now we can fill the array in one class and search in another class
         //But we need a menu to be show always until the user wanted to exit so for the first time we can do
-        String flag = "y";
+        String flag = "Y";
         do{
-            System.out.println("Options with the array: \n");
-            System.out.println("1. Fill the array");
-            System.out.println("2. Search an element\n");
+            System.out.println("\n----------------------------------------------------------");
+            System.out.println("Options with the array:");
+            System.out.println("----------------------------------------------------------\n");
+            System.out.println("1. Fill all the array (just if has no use it 2nd option)");
+            System.out.println("2. Fill the array with one element");
+            System.out.println("3. Search an element");
+            System.out.println("4. See the elements of the array");
+            System.out.println("5. Exit");
+            System.out.println("----------------------------------------------------------\n");
             
-            int option = Integer.parseInt(System.console().readLine());
-
+            int option = Integer.parseInt(scanner.nextLine());
+            int element;
             switch (option) {
                 case 1:
                     fillArray(array);
+                    System.out.println("Array fill up");
                     break;
-            
                 case 2:
+                    System.out.println("Type the element to put inside");
+                    element = Integer.parseInt(scanner.nextLine());
+                    filloneArray(array, element);
+                    break;
+                case 3:
                     System.out.println("Type the element to find");
-                    int element = Integer.parseInt(System.console().readLine());
-
-                    int temp = findElement(array, element, index);
+                    element = Integer.parseInt(scanner.nextLine());
+                    int temp = findElement(array, element);
                     if (temp!=-1){
                         System.out.println("The index of the element is: " + temp);
                     }else{
                         System.out.println("Element is not on the array");
                     }
-
+                    break;
+                case 4:
+                    //Print the array that is on the moment and not just empty slots
+                    printArray(array);
+                    break;
+                case 5:
+                    flag = "N";
                     break;
             }
-            //Ask if wanterd to continue
-            System.out.println("Continue or exit: [Y, N]");
-            flag = System.console().readLine();
-        }while(flag.equals("y") || flag.equals("Y"));
+        }while(flag.equals("Y"));
         System.out.println("Later");
     }
 
 
     //Method to fill the array with values
     public static void fillArray(int[] array) {
-        //Check if array is null
-        if(array == null) {
+        //Check if array is full
+        if(count >= array.length) {
             System.out.println("The array is full");
             return;
         }else{
@@ -59,26 +77,51 @@ public class Main {
             for (int i=0; i < array.length; i++) {
                 System.out.println("Element: "+ (i+1));
                 array[i] = Integer.parseInt(System.console().readLine());
+                count++;
             }
         }
     }
 
+    //Method to put one element at time
+    public static void filloneArray(int [] array, int element){
+        //Chewck if array is full
+        if(count>= array.length){
+            System.out.println("Array is full");
+            return;
+        }
+        array[count] = element;
+        count++;
+        System.out.println("Element: "+element+" added to the array");
+    } 
+
+    public static void printArray(int [] array){
+        if(count==0){
+            System.out.println("There is no elements on the array");
+            return;
+        }
+        for(int i=0; i<count; i++){
+            System.out.println("|"+"["+array[i]+"]"+"|");
+        }
+    }
+
     //Method to find an element in the array
-    public static int findElement(int[] array, int element, int index) {
+    public static int findElement(int[] array, int element) {
         //Check if is not empty
-        if(array == null ){
+        if(count == 0 ){
             System.out.println("Array is empty");  
+            return -1;
         }else{
             //Binary Search, need to have the array order
 
             //Order the array first
-            java.util.Arrays.sort(array);
+            //This is for example if the array has lenght 100000, and you only have fill 3 slots, this will only order 0 - 3, and not the rest
+            java.util.Arrays.sort(array, 0, count);
 
-            int mid=0, low=0, high=array.length;
+            int mid = 0, low=0, high=count-1;
             while(low<=high){
                 mid=(low+high)/2;
                 if(array[mid]== element){
-                    return index=mid;
+                    return mid;
                 }else if (array[mid]<element){
                     low = mid+1;
                 }else if (array[mid]>element) {
@@ -86,7 +129,7 @@ public class Main {
                 }
             }
         }
-        return index;
+        return -1;
     }
 }
 
