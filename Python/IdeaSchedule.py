6328a90bc90fdfc0c0c0a982ca72da64dfd91e34
DWgg <308503815+diwsgg@users.noreply.github.com>
2026-08-20 13:04:36 -0600

    Adding changes to python files

---
+++ Python/IdeaSchedule.py
@@ -0,0 +1,17 @@
+
+'''
+The idea is to asked to the user, the time he will start an activity
+and the amount of time he would like to spent on that activity
+
+Like work, that are most of the time 8hrs, if he starts at 7am when he will stop, (including the hour to eat)
+so 7 am + 8hrs work + 1hr to eat --> 4 pm
+
+We can asked to the user, 
+Start time, Hours to spent on the activity, Hours to eat,(And after consider the time of the activity, we can choose when is the best time to eat, iof needed)
+
+This can be display with a temporizer if he would like  putting animations, or changing the time color, depending on the time of the day.
+
+In mexico after 6fhrs of consecutive work, the workers have right to eat.
+'''
+
+
