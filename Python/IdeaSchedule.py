9ef8c050cf5661d166e057816c835bc06ea0b0d4
DWgg <308503815+diwsgg@users.noreply.github.com>
2026-08-27 12:30:00 -0600

    Revert "Asking the information to the user"
    This reverts commit d40c5fefd9f971175b7e3f6e2c4dcbb6b88a069a.

--- Python/IdeaSchedule.py
+++ Python/IdeaSchedule.py
@@ -15,24 +15,3 @@ In mexico after 6fhrs of consecutive work, the workers have right to eat.
 '''
 
 
-print("Welcome to the Schedule Visualizier")
-
-#String ? 1:30, its a string but we can divided by hour and minutes for a best manage
-
-start_time_hours = int(input("What hour are you going to start the activity: (Just the number like 1, 12, 7)"))
-
-while start_time_hours <=0 or start_time_hours>= 13:
-    print("Invalid hours")
-    start_time_hours = int(input("Type the start hours again"))
-
-
-start_time_mins = int("Also the minutes that you going to start the activity: (Just the number like 20, 30, 45)")
-
-while start_time_mins <0 or start_time_mins > 60:
-    print("Invalid minutes")
-    start_time_mins = int(input("Type the start minutes again"))
-
-#So we have to decide also the time like if the user put 12 next hour is 1pm or could be 1am depending the shift
-
-shift = int(input("What shift are you working on [1.'AM', 2.'PM']"))
-
