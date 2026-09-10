e669bb6151f22db5f65eadc2fe89c85f1776e7b5
DWgg <308503815+diwsgg@users.noreply.github.com>
2026-09-10 11:52:43 -0600

    Moving the snake

--- Python/day20/main.py
+++ Python/day20/main.py
@@ -13,7 +13,19 @@ screen.tracer(0)
 
 snake = Snake()
 
+#We need to listen the user (keys to move)
+screen.listen()
+#We need to define a function to detect when we move
+
+#ON KEY TAKES TWO ARGUMENTS, a funct and a key, the function can be anything to determinate the movement of that key
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_on = True
+
+#We are moving always, but the scree.onkey with list can be executed
 while game_on:
     screen.update()
     time.sleep(0.1)
--- Python/day20/snake.py
+++ Python/day20/snake.py
@@ -2,12 +2,21 @@ from turtle import Turtle
 
 STARTING_POSITIONS=[(0,0),(-20,0),(-40,0),]
 MOVE_DISTANCE = 20
+#ANGLES
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
 
 class Snake:
     def __init__(self):
         self.allturtles = []
+        #We have created an array, and at same tim, we put the snakes into the array
         self.create_snake()
-
+        #WE put the "head" as the first turtle created
+        self.head = self.allturtles[0]
+    
     def create_snake(self):
         for positions in STARTING_POSITIONS:
             newTurtles = Turtle(shape="square")
@@ -18,8 +27,29 @@ class Snake:
 
 #movement
     def move(self):
+        #we move first all the tail and then the head
+        #if we move the head first 
         for seg_num in range(len(self.allturtles)-1, 0, -1):
             new_x = self.allturtles[seg_num-1].xcor()
             new_y = self.allturtles[seg_num-1].ycor()
             self.allturtles[seg_num].goto(new_x,new_y)
-        self.allturtles[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    #Directions
+    #If the direction is the opposite we can not change our direction
+    def up(self):
+        #This means we can move up when we are like -> or <- but not down
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
